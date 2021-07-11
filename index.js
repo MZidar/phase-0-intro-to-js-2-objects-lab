cdc8605c3e8567d6a271155656a770219bfc9165
Michael Zidar <mzidar@gmail.com>
2021-07-11 17:17:19 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution in this file!
+const employee = {
+    name: `Mike Z`,
+    street: `405 17th St`
+};
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newemp1 = { ...employee };
+  
+    newemp1[key] = value;
+  
+    return newemp1;
+  }
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    
+  
+    employee[key] = value;
+  
+    return employee;
+  }
+
+  function deleteFromEmployeeByKey(employee, key) {
+    const removeemp = { ...employee };
+  
+    delete removeemp[key];
+  
+    return removeemp;
+  }
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
